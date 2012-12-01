4643761d924359376137dee7892fa9a85d45030b
ahmad <ahmad.al.hajjar@hotmail.fr>
2012-12-01 18:28:26 +0100

    boosteteigen j'ai transforme' la matrice importe' du fichier CSV (qui est en string)a une matrice en float

--- ahmad/boosteteigen.cpp
+++ ahmad/boosteteigen.cpp
@@ -9,6 +9,7 @@
 #include <iostream>     // cout, endl
 #include <fstream>      // fstream
 #include <boost/tokenizer.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string.hpp>
 #include "/usr/include/eigen2/Eigen/LU"// provides LU decomposition
 #include "/usr/include/eigen2/Eigen/Array"// provides random matrices
@@ -20,7 +21,7 @@ int main()
     using namespace Eigen;
     /* ************************************************************* */
     /*//recuperation du document csv//*/
-    string data("coord.csv");
+    string data("mat_eta.csv");
 
     ifstream in(data.c_str());
     if (!in.is_open()) return 1;
@@ -29,12 +30,16 @@ int main()
     typedef Matrix<float, 196, 194> Matrice1;
     /* ************************************************************* */
     vector< string > vec;
+     vector< float > vecf;
     vector<string> splitArray;
-    vector<string> splitArrayd;
+    vector<float> splitArrayd;
     vector< vector<string> > matrice;
+    vector< vector<float > > matricef;
     vector< string>::iterator iter_jj;
     vector< string>::iterator it;
     vector< vector<string> >::iterator iter_ii;
+    vector< float>::iterator iter_jjf;
+    vector< vector<float> >::iterator iter_iif;
     Matrice1 m ;
     string line;
     /* ************************************************************* */
@@ -44,6 +49,19 @@ int main()
         vec.assign(tok.begin(),tok.end());
     /*// decomposition du vecteur obtenu et on le met dans un autre vecteur//*/
         split(splitArray, vec.at(0), is_any_of(";"));
+   //cout<<splitArray.size()<<"\n";
+        int TempNumOne=splitArray.size();
+    for (int a=0;a<TempNumOne;a++){
+    //splitArrayd.push_back(lexical_cast<float>(splitArray[a]));
+            try{
+            splitArrayd[a]= boost::lexical_cast<float>(splitArray[a]);
+            }
+            catch(bad_lexical_cast &){
+            splitArrayd.push_back(0);
+            }
+    //cout<< splitArrayd[a];
+        }
+
     /* ************************************************************* */
     /*//conversion de la matrice en float//*/
         /*string tmp;
@@ -53,19 +71,24 @@ int main()
             float  flo = strtof(tmp.c_str(), NULL);
             cout<<flo<<"\t";
             splitArrayd.push_back(flo);
-            cout<< splitArrayd.at(0)<<"\n";
-            cout<<splitArrayd.at()<<"\n";
-        }
-        cout<<splitArrayd.at(2)<<"\n";
-        cout<<splitArrayd.at()<<"\n";
-        */
+        }*/
 
         matrice.push_back(splitArray);
+        matricef.push_back(splitArrayd);
     }
+        cout<<splitArray.size()<<"\n";
+        cout<<splitArrayd.size()<<"\n";
+
         matrice.erase(matrice.begin());
+        matricef.erase(matricef.begin());
+
+        cout<<matrice.size()<<"\n" ;
+        cout<<matricef.size()<<"\n";
+
+
        /* ************************************************************* */
     /*//Affichage de la matrice//*/
-    /*for(iter_ii=matrice.begin(); iter_ii!=matrice.end(); iter_ii++){
+ /*  for(iter_ii=matrice.begin(); iter_ii!=matrice.end()-(matrice.size()-1); iter_ii++){
 
           for(iter_jj=(*iter_ii).begin(); iter_jj!=(*iter_ii).end(); iter_jj++){
 
@@ -74,7 +97,17 @@ int main()
            }
         cout << "\n----------------------" << endl;
         }
-    */
+
+       for(iter_iif=matricef.begin(); iter_iif!=matricef.end()-(matricef.size()-1); iter_iif++){
+
+          for(iter_jjf=(*iter_iif).begin(); iter_jjf!=(*iter_iif).end(); iter_jjf++){
+
+             cout << *iter_jjf<<"\t";
+
+           }
+        cout << "\n----------------------" << endl;
+        }*/
+
         }
     /*
     cout << "Here is, up to permutations, its LU decomposition matrix:"
