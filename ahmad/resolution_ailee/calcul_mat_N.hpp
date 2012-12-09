3c6ffc16b52330f2585b96d17c9a9145363dcc79
Ahmad <ahmad.al.hajjar@hotmail.fr>
2012-12-09 20:12:26 +0100

    resolution ailee a peut pres fonctionnel + code matlab

---
+++ ahmad/resolution_ailee/calcul_mat_N.hpp
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                 -------------------------                      //
+//                    calcul_mat_N.hpp                            //
+//                 -------------------------                      //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+#ifndef __projet_pfe__Calcul_mat_N__
+#define __projet_pfe__Calcul_mat_N__
+
+#include "allincludes.hpp"
+
+
+    Matriceinv calculmatriceN2N4 (int h , Matrice r , Matrice alpha2 );
+    MatriceN matricen (int h , int k ,  Matrice d , Matrice alpha1 , Matrice v);
+
+
+
+
+
+
+#endif
